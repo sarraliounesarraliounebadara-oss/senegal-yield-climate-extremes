4f0d3b97881f96bc31215e9a894cd5a18526964d
sarraliounesarraliounebadara-oss <sarr.aliounesarr.aliounebadara@gmail.com>
2026-06-11 22:56:43 +0200

    03_interpolation_to_hdr-files.py

--- 03_extract_mirca_cultivated_pixels.py
+++ 03_extract_mirca_cultivated_pixels.py
@@ -1,4 +1,37 @@
-# =========================================
+import os
+import glob
+
+MIRCA_DIR = (
+    "/media/sarr/01DC5DE9D15E8CF0/"
+    "departments/agro_data/mask/monthly_growing_area_grids"
+)
+
+hdr_text = """NCOLS 720
+NROWS 360
+XLLCORNER -180
+YLLCORNER -90
+CELLSIZE 0.5
+NODATA_VALUE -9999
+NBITS 32
+PIXELTYPE FLOAT
+BYTEORDER LSBFIRST
+"""
+
+files = glob.glob(
+    os.path.join(MIRCA_DIR, "*.flt")
+)
+
+for flt in files:
+
+    hdr = flt.replace(".flt", ".hdr")
+
+    with open(hdr, "w") as f:
+
+        f.write(hdr_text)
+
+    print("Created:", hdr)
+
+print("\nDONE")# =========================================
 # MIRCA MASK - CULTIVATED PIXELS EXTRACTION
 # =========================================
 
